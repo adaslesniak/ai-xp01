e53a1966db5592bf054f80073cc9b88a87c20855
Adas Lesniak <radical.ignorant@gmail.com>
2024-02-05 20:18:19 +0100

    much deeper model isn't any better

--- NnModel.py
+++ NnModel.py
@@ -5,11 +5,13 @@ from tensorflow.keras.optimizers import SGD
 
 def prepare_model():
     model = Sequential([
-        Dense(64, input_shape=(25,), activation='relu'),  # First hidden layer with 64 neurons
-        Dense(64, activation='relu'),  # Second hidden layer with 64 neurons
+        Dense(96, input_shape=(25,), activation='relu'),  # First hidden 
+        Dense(128, activation='relu'),  # Second hidden 
+        Dense(96, activation='relu'),  # Third hidden 
+        Dense(48, activation='relu'),  # Third hidden 
         Dense(1, activation='sigmoid')  # Output layer with sigmoid activation for binary classification
     ])
-    sgd_optimizer = SGD(lr=0.01, momentum=0.9)
+    sgd_optimizer = SGD(learning_rate=0.007, momentum=0.66)
     model.compile(optimizer=sgd_optimizer, 
                   loss='binary_crossentropy', 
                   metrics=['accuracy'])
--- Training.py
+++ Training.py
@@ -1,23 +1,22 @@
 from NnModel import prepare_model
 from DataGenerator import load_training_data, load_test_data
 
-model = None
 
 def train():
     training_data, training_labels = load_training_data()
     model = prepare_model()
     history = model.fit(training_data, 
                         training_labels, 
-                        epochs=75, batch_size=32, validation_split=0.2)
+                        epochs=88, batch_size=24, validation_split=0.16)
     print(history)
     return model
 
 
-def evaluate(trained_model):
+def evaluate_accuracy(trained_model):
     test_data, test_labels = load_test_data()
     test_loss, test_acc = trained_model.evaluate(test_data, test_labels)
     print(f"Test Accuracy: {test_acc*100:.2f}%, loss: {test_loss}")
 
 
 the_thing = train()
-evaluate(the_thing)
+evaluate_accuracy(the_thing)
