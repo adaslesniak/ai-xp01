28b2c718f5894018dd73d98600c512291dcc92b6
Adas Lesniak <radical.ignorant@gmail.com>
2024-02-05 21:23:37 +0100

    regularization kills it

--- NnModel.py
+++ NnModel.py
@@ -1,15 +1,16 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.regularizers import l2
 
 
 def prepare_model():
     model = Sequential([
         Dense(64, input_shape=(25,), activation='relu'),  # First hidden layer with 64 neurons
-        Dense(64, activation='relu'),  # Second hidden layer with 64 neurons
+        Dense(64, activation='relu', kernel_regularizer=l2(0.01)),  # Second hidden layer with 64 neurons
         Dense(1, activation='sigmoid')  # Output layer with sigmoid activation for binary classification
     ])
-    the_optimizer = RMSprop(learning_rate=0.001)
+    the_optimizer = RMSprop(learning_rate=0.002)
     model.compile(optimizer=the_optimizer, 
                   loss='binary_crossentropy', 
                   metrics=['accuracy'])
--- Training.py
+++ Training.py
@@ -5,10 +5,9 @@ from DataGenerator import load_training_data, load_test_data, generate_data
 def train():
     training_data, training_labels = load_training_data()
     model = prepare_model()
-    history = model.fit(training_data, 
+    _ = model.fit(training_data, 
                         training_labels, 
-                        epochs=99, batch_size=32, validation_split=0.2)
-    print(history)
+                        epochs=66, batch_size=48, validation_split=0.17)
     return model
 
 
@@ -18,6 +17,6 @@ def evaluate(trained_model):
     print(f"Test Accuracy: {test_acc*100:.2f}%, loss: {test_loss}")
 
 
-generate_data(99, 16)
+#generate_data(99, 16)
 the_thing = train()
 evaluate(the_thing)
