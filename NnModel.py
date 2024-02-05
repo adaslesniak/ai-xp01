3a7e32f0b2715924f28c8c1b7eac9fb92cb8c93b
Adas Lesniak <radical.ignorant@gmail.com>
2024-02-05 20:49:15 +0100

    Revert "swish doesn't work as well - 75%"
    This reverts commit 2f5815f379a8fccd6d2064722aab20749b13aaa1.

--- NnModel.py
+++ NnModel.py
@@ -6,9 +6,9 @@ from tensorflow.keras.optimizers import SGD
 def prepare_model():
     model = Sequential([
         Dense(96, input_shape=(25,), activation='elu'),  # First hidden 
-        Dense(128, activation='swish'),  # Second hidden 
-        Dense(96, activation='swish'),  # Third hidden 
-        Dense(48, activation='swish'),  # Third hidden 
+        Dense(128, activation='elu'),  # Second hidden 
+        Dense(96, activation='elu'),  # Third hidden 
+        Dense(48, activation='elu'),  # Third hidden 
         Dense(1, activation='sigmoid')  # Output for binary classification
     ])
     sgd_optimizer = SGD(learning_rate=0.007, momentum=0.66)
